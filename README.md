c91dfbaae41f42743c825e82eaee30863cef6952
akjca <adamkjensen@gmail.com>
2014-07-17 20:07:12 -0400

    0.5

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 <!--- 	koenkrawl spider	---!>
 
-I wrote this for fun one evening. It serves no real purpose.
+I wrote this for fun one evening. It serves no real purpose. 
 Credit to the forum post (that i have since lost) that inspired the code.
 it will throw exceptions instead of something awesome like "Krawl Komplete!"
+Encoding errors means its done.
 url syntax = www.anysite.tld/
 without / it seems to crawl only http links. with / crawls local directories aswell.
 
-run koen-X.X.py 
+run koen.py 
 
-follow with tail -f otp.txt
+follow with tail -f otp
 
-copy/rename otp.txt
+cp otp
 
 rinse, repeat.
+
+
 
 
---
+++ koen.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import urllib, urllib2, htmllib, formatter, re, sys, os, socket
+print '<!---    koenkrawl spider    ---!> \n made by gnomes out of discarded snake skin.'
+url = raw_input("enter site to krawl: ")
+print 'be patient...'
+f = open('otp','wb')
+w = urllib.urlopen("http://"+url)
+w.addheaders = [('User-agent', 'KoenKrawler/0.5')]
+d = w.read()
+w.close()
+format = formatter.AbstractFormatter(formatter.NullWriter())
+t = htmllib.HTMLParser(format)
+t.feed(d)
+link = []
+links = t.anchorlist
+for link in links:
+	if re.search('http', link) != None:		
+		f.write(link+'\n') 
+		w2 = urllib.urlopen(link)
+		d2 = w2.read()
+		w2.close()
+		format = formatter.AbstractFormatter(formatter.NullWriter())
+		t = htmllib.HTMLParser(format)
+		t.feed(d2)
+		alink = []
+		blinks = t.anchorlist
+		for alink in blinks:
+			if re.search('http', link) != None:
+				links.append(alink)
+				f.write(alink+'\n') 
+			
+f.close()
+print 'The site returned no links'
+print 'probably...'
